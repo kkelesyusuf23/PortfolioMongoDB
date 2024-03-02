6146ac50718ff418260326ec0d1b563201cf113d
kkelesyusuf <kkelesyusuf23@gmail.com>
2024-03-02 19:50:57 +0300

    ...

---
+++ PortfolioMongoDB/Controllers/FeatureController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using PortfolioMongoDB.DAL.Entities;
+using PortfolioMongoDB.DAL.Settings;
+
+namespace PortfolioMongoDB.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly IMongoCollection<Feature> _featureCollection;
+        public FeatureController(IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _featureCollection = database.GetCollection<Feature>(_databaseSettings.FeatureCollectionName);
+        }
+        public async Task<IActionResult> Index()
+        {
+            var values =await  _featureCollection.Find(x => true).ToListAsync();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateFeature()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(Feature feature)
+        {
+            await _featureCollection.InsertOneAsync(feature);
+            return RedirectToAction("Index");
+        }
+    }
+}
